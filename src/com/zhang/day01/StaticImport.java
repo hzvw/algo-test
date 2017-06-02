5c41f26a295e91faacb5e442219882b11a79e126
zhanghz1120 <zhanghz1120@gmail.com>
2017-06-03 01:06:06 +0800

    创建项目

---
+++ src/com/zhang/day01/StaticImport.java
@@ -0,0 +1,12 @@
+package com.zhang.day01;
+
+import static java.lang.Math.*;
+/**
+ * 静态导入
+ */
+public class StaticImport {
+    public static void main(String[] args){
+        System.out.println(max(1, 2));
+    }
+
+}
