5a6d8975e78570ee78d3e31f3a92bb995a8d0e7c
zhanghz1120 <zhanghz1120@foxmail.com>
2025-01-16 21:23:43 +0800

    0116于广州

---
+++ src/com/zhang/od/_022_.java
@@ -0,0 +1,59 @@
+package com.zhang.od;
+
+import java.util.*;
+
+/**
+ * ClassName: _022_
+ * Package: com.zhang.od
+ * Description:
+ *
+ * @Author 张汉至
+ * @Create 2025/1/16 18:57
+ * @Version 1.0
+ */
+public class _022_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = Integer.parseInt(sc.nextLine());
+        int x = Integer.parseInt(sc.nextLine());
+        LinkedList<LinkedList<Integer>> arr = new LinkedList<>();
+        int cnt = 0;
+        for(int i = 0; i<x; i++){
+            LinkedList<Integer> i1 = new LinkedList<>();
+            String line = sc.nextLine();
+            String[] words = line.split(",");
+            for(int j = 0; j<words.length; j++){
+                if(words[j].equals("")){
+                    continue;
+                }
+                i1.add(Integer.parseInt(words[j]));
+                cnt++;
+            }
+            arr.add(i1);
+        }
+
+        List<Integer> buffer = new ArrayList<>();
+        int i = 0;
+        while(buffer.size() < cnt){
+            int j = 0;
+            while(j < len && arr.get(i).size() != 0){
+                int val = arr.get(i).removeFirst();
+                buffer.add(val);
+                j++;
+            }
+            i++;
+            if(i >= arr.size()){
+                i = 0;
+            }
+        }
+
+        List<String> buffer2 = new ArrayList<>();
+        buffer.forEach(k -> buffer2.add(k + ""));
+        System.out.println(String.join(",", buffer2));
+
+    }
+
+
+
+
+}
---
+++ src/com/zhang/od/_023_.java
@@ -0,0 +1,37 @@
+package com.zhang.od;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+/**
+ * ClassName: _023_
+ * Package: com.zhang.od
+ * Description:
+ *
+ * @Author 张汉至
+ * @Create 2025/1/16 19:19
+ * @Version 1.0
+ */
+public class _023_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == 'o'){
+                set.add(i);
+            }
+        }
+
+        if(set.size() % 2 == 0){
+            System.out.println(s.length());
+        }else{
+            System.out.println(s.length() - 1 );
+        }
+
+
+
+    }
+
+}
