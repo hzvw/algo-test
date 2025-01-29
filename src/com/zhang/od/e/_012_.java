353bb658fd64ed7572312dfd6d3ec30eb7a50f8e
zhanghz1120 <zhanghz1120@foxmail.com>
2025-01-29 18:14:12 +0800

    一天做一道题

---
+++ src/com/zhang/od/e/_012_.java
@@ -0,0 +1,19 @@
+package com.zhang.od.e;
+
+/**
+ * ClassName: _012_
+ * Package: com.zhang.od.e
+ * Description:
+ *
+ * @Author 张汉至
+ * @Create 2025/1/29 2:18
+ * @Version 1.0
+ */
+public class _012_ {
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
