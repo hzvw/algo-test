63a857b638383b8e878fb08a8b8b0cedad7c7d92
zhanghz1120 <zhanghz1120@foxmail.com>
2025-02-11 15:14:39 +0800

    0211于广州

--- src/com/zhang/od/e/_042_.java
+++ src/com/zhang/od/e/_042_.java
@@ -16,40 +16,34 @@ public class _042_ {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-
-        //要重复子串的起始位置，在sb中的起始位置
-        Stack<Integer> idx = new Stack<>();
-        //要重复子串的重复次数
-        Stack<Integer> numStack = new Stack<>();
-
-        //保存最终的结果
         StringBuilder sb = new StringBuilder();
-        //暂时存储数字
-        StringBuilder num = new StringBuilder();
-        //暂时存储要重复的字符串
-        StringBuilder repeat = new StringBuilder();
+        //记录要重复的子串在sb中的起始位置
+        Stack<Integer> ids = new Stack<>();
+        //记录要重复子串的次数
+        Stack<Integer> nums = new Stack<>();
+
+        String repeatStr = "";
+        String num = "";
         char[] cs = str.toCharArray();
         for (int i = 0; i < cs.length; i++) {
             char c = cs[i];
-
             if(c == '['){
-                //说明数字结束
-                if(num.length() > 0){
-                    numStack.push(Integer.parseInt(num.toString()));
-                    num = new StringBuilder();
+                if(!"".equals(num)){
+                    nums.push(Integer.parseInt(num));
+                    num = "";
                 }
-                idx.push(sb.length());
+                int start = sb.length();
+                ids.push(start);
             }else if(c == ']'){
-                int num_i = numStack.pop();
-                int start = idx.pop();
-                String tmp = sb.substring(start);
-                String tmp2 = "";
-                for (int j = 0; j < num_i; j++) {
-                    tmp2 += tmp;
+                int start = ids.pop();
+                int num2 = nums.pop();
+                for (int j = 0; j < num2; j++) {
+                    repeatStr += sb.substring(start);
                 }
-                sb.replace(start, sb.length(), tmp2);
-            }else if(c >= '0' && c <= '9'){
-                num.append(c);
+                sb.replace(start, sb.length(), repeatStr);
+                repeatStr = "";
+            }else if(c >= '0' && c<='9'){
+                num += c;
             }else{
                 sb.append(c);
             }
