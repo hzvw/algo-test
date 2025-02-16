b74c4ceef4c6d91c082fa46a46a7a15a3f6f2ddd
zhanghz1120 <zhanghz1120@foxmail.com>
2025-02-16 18:00:27 +0800

    数位dp，记忆化搜索

--- src/com/zhang/od/e/_076_.java
+++ src/com/zhang/od/e/_076_.java
@@ -15,25 +15,24 @@ import java.util.concurrent.CountDownLatch;
  */
 public class _076_ {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int l = sc.nextInt();
         int r = sc.nextInt();
 
         int ans = compute(r) - compute(l-1);
-
         System.out.println(ans);
-
     }
 
     static int compute(int i){
         String str = Integer.toBinaryString(i);
 
-        int[][][] dp = new int[str.length()][2][2];
-        return dfs(true, 0, str.length(), str, dp,0,0);
+        int n = str.length();
+        int[][][] dp = new int[n][2][2];
+
+        return dfs(0, true, n, str, dp, 0,0);
     }
 
-    static int dfs(boolean limit, int level, int n, String str, int[][][] dp, int pre, int prepre){
+    static int dfs(int level, boolean limit, int n, String str, int[][][] dp, int pre, int prepre){
         if(level == n){
             return 1;
         }
@@ -43,20 +42,19 @@ public class _076_ {
         }
 
         int count = 0;
-        int max = limit ? (str.charAt(level) - '0') : 1;
-        for (int i = 0; i <=max; i++) {
+        int max = limit ? (str.charAt(level)) : 1;
+        for (int i = 0; i <= max; i++) {
             if(i == 1 && pre == 0 && prepre == 1){
                 continue;
             }
-            count += dfs(limit && i == max, level+1, n, str, dp, i, pre);
+            count += dfs(level+1, limit && i == max, n, str, dp, i, pre);
         }
-
         if(!limit){
             dp[level][pre][prepre] = count;
         }
+
         return count;
     }
-
 
 
 }
