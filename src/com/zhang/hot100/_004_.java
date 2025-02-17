8e8b615f4d1e2c8d8ca82d1b1d54dc4609848d78
hzvw <zhanghz1120@gmail.com>
2025-02-18 01:17:43 +0800

    hot100-移动零

---
+++ src/com/zhang/hot100/_004_.java
@@ -0,0 +1,39 @@
+package com.zhang.hot100;
+
+/**
+ * ClassName: _004_
+ * Package: com.zhang.hot100
+ * Description:
+ *
+ * @Author Harizon
+ * @Create 2025/2/18 1:00
+ * @Version 1.0
+ */
+public class _004_ {
+    public static void main(String[] args) {
+        new _004_().moveZeroes(new int[]{0,1,0,3,12});
+
+    }
+
+    public void moveZeroes(int[] nums) {
+        int[] count = new int[nums.length];
+
+        int n = nums.length;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if(nums[i] == 0){
+                cnt++;
+            }
+            count[i] = cnt;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(nums[i] != 0){
+                nums[i-count[i]] = nums[i];
+            }
+        }
+        for (int i = n-count[n-1]; i < n; i++) {
+            nums[i] = 0;
+        }
+    }
+}
