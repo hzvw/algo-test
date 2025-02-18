8a079a03725c7703fb09cdb3e2f659593c1e267e
hzvw <zhanghz1120@gmail.com>
2025-02-18 15:55:38 +0800

    hot100-找到字符串中所有字母异位词

---
+++ src/com/zhang/hot100/_009_.java
@@ -0,0 +1,101 @@
+package com.zhang.hot100;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * ClassName: _009_
+ * Package: com.zhang.hot100
+ * Description:找到字符串中所有字母异位词
+ *
+ * @Author Harizon
+ * @Create 2025/2/18 14:18
+ * @Version 1.0
+ */
+public class _009_ {
+    public static void main(String[] args) {
+
+        String s = "cbaebabacd";
+        String t = "abc";
+        List<Integer> list = new _009_().findAnagrams(s, t);
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+    }
+
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] count = new int[26];
+        int n = p.length();
+        int k = n;
+        for (int i = 0; i < n; i++) {
+            count[p.charAt(i)-'a']++;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (j < s.length()){
+            char c = s.charAt(j);
+            if(count[c-'a'] > 0){
+                k--;
+            }
+            count[c-'a']--;
+
+            while (k == 0){
+                if(j - i + 1 == n){
+                    res.add(i);
+                }
+                char c2 = s.charAt(i);
+                if(count[c2 - 'a'] == 0){
+                    k++;
+                }
+                count[c2 - 'a']++;
+                i++;
+            }
+            j++;
+        }
+        return res;
+    }
+
+    public String minWindow(String s, String t) {
+        int n = t.length();//目标长度
+
+        // 统计每个字符的次数
+        int[] count = new int[128];
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i)]++;
+        }
+
+        int i = 0;
+        int j = 0;
+        //双指针就绪
+
+        int min_len = s.length() + 1;
+        int start = 0;//记录最终结果的起点
+        while(j < s.length()){
+            char c = s.charAt(j);
+            if(count[c] > 0){
+                n--;
+            }
+            count[c]--;
+
+            while (n == 0){
+                //说明i能向右收缩
+                if(min_len > j-i+1){
+                    min_len = j-i+1;
+                    start = i;
+                }
+                char tmp = s.charAt(i);
+                if(count[tmp] == 0){
+                    n++;
+                }
+                count[tmp]++;
+
+                i++;
+            }
+            j++;
+        }
+        return min_len == s.length()+1 ? "" : s.substring(start, start+min_len);
+    }
+}
