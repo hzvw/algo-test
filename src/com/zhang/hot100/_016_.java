8d0d86a5d7e1c9f0ab8c807fd4a0eedda3e42a6c
hzvw <zhanghz1120@gmail.com>
2025-02-18 18:29:28 +0800

    hot100-除自身以外数组的乘积

---
+++ src/com/zhang/hot100/_016_.java
@@ -0,0 +1,36 @@
+package com.zhang.hot100;
+
+/**
+ * ClassName: _016_
+ * Package: com.zhang.hot100
+ * Description:
+ *
+ * @Author Harizon
+ * @Create 2025/2/18 18:15
+ * @Version 1.0
+ */
+public class _016_ {
+    public static void main(String[] args) {
+
+
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+
+        int[] ans = new int[n];
+
+        int l = 1;
+        for (int i = 0; i < n; i++) {
+            ans[i] = l;
+            l = l * nums[i];
+        }
+
+        int r = 1;
+        for (int i = n-1; i >=0 ; i--) {
+            ans[i] = ans[i] * r;
+            r = r * nums[i];
+        }
+        return ans;
+    }
+}
