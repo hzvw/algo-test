ead3456c1f928bcf7aadc0fb600a63b109ea8da2
hzvw <zhanghz1120@gmail.com>
2025-02-20 01:02:35 +0800

    hot100-路径总和三

--- src/com/zhang/hot100/_048_.java
+++ src/com/zhang/hot100/_048_.java
@@ -1,5 +1,7 @@
 package com.zhang.hot100;
 
+import java.util.HashMap;
+
 /**
  * ClassName: _048_
  * Package: com.zhang.hot100
@@ -14,6 +16,36 @@ public class _048_ {
 
 
 
+    }
+
+    int ans = 0;
+
+    HashMap<Long, Integer> map = new HashMap<>();
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return 0;
+        }
+
+        map.put(0l, 1);
+        dfs(root, root.val, targetSum);
+
+        return ans;
+    }
+
+    void dfs(TreeNode root, long val, int target){
+        if(map.containsKey(val - target)){
+            ans += map.get(val - target);
+        }
+
+        map.put(val, map.getOrDefault(val, 0)+1);
+        if(root.left != null){
+            dfs(root.left, val + root.left.val, target);
+        }
+        if(root.right != null){
+            dfs(root.right, val + root.right.val, target);
+        }
+
+        map.put(val, map.getOrDefault(val, 0)-1);
     }
 
 }
