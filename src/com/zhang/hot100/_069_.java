a3317fd8ae4267a93f4030147b33e0f489487e97
hzvw <zhanghz1120@gmail.com>
2025-02-21 01:01:41 +0800

    hot100-有效的括号

---
+++ src/com/zhang/hot100/_069_.java
@@ -0,0 +1,39 @@
+package com.zhang.hot100;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * ClassName: __068
+ * Package: com.zhang.hot100
+ * Description:
+ *
+ * @Author Harizon
+ * @Create 2025/2/21 0:23
+ * @Version 1.0
+ */
+public class _069_ {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(c == '('){
+                stack.push(')');
+            }else if(c == '['){
+                stack.push(']');
+            }else if(c == '{'){
+                stack.push('}');
+            }else{
+                if(stack.isEmpty() || stack.peek() != c){
+                    return false;
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
