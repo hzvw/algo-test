d5415f36f281b65fedd7bce2ac56d8c493b0a0a6
hzvw <zhanghz1120@gmail.com>
2025-02-20 16:27:06 +0800

    hot100-全排列

---
+++ src/com/zhang/hot100/_055_.java
@@ -0,0 +1,51 @@
+package com.zhang.hot100;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * ClassName: _055_
+ * Package: com.zhang.hot100
+ * Description:
+ *
+ * @Author Harizon
+ * @Create 2025/2/20 16:21
+ * @Version 1.0
+ */
+public class _055_ {
+
+    public List<List<Integer>> permute(int[] nums) {
+        this.nums = nums;
+        visited = new boolean[nums.length];
+        dfs(0, nums.length, new LinkedList<>());
+        return res;
+    }
+
+    int[] nums;
+    List<List<Integer>> res = new ArrayList<>();
+
+    boolean[] visited;
+
+    void dfs(int level, int n, LinkedList<Integer> buckets){
+        if(level == n){
+            res.add(new ArrayList<>(buckets));
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(visited[i]){
+                continue;
+            }
+            buckets.addLast(nums[i]);
+            visited[i] = true;
+
+            dfs(level+1, n, buckets);
+
+            buckets.removeLast();
+            visited[i] = false;
+        }
+
+    }
+
+}
