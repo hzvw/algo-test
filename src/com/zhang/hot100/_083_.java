157126084a4f66953bfad3793d175c3b96d0fa3c
hzvw <zhanghz1120@gmail.com>
2025-02-22 01:15:49 +0800

    hot100-打家劫舍

---
+++ src/com/zhang/hot100/_083_.java
@@ -0,0 +1,34 @@
+package com.zhang.hot100;
+
+import java.util.Arrays;
+
+/**
+ * ClassName: _083_
+ * Package: com.zhang.hot100
+ * Description:
+ *
+ * @Author Harizon
+ * @Create 2025/2/22 0:52
+ * @Version 1.0
+ */
+public class _083_ {
+
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int n = nums.length;
+        if(n < 2){
+            return nums[0];
+        }
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+
+        return dp[n-1];
+    }
+}
