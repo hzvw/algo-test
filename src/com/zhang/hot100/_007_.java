c0fe24d20becb8beca5a5051e482a6e22fac973f
hzvw <zhanghz1120@gmail.com>
2025-02-23 05:07:38 +0800

    hot100-接雨水，单调栈

--- src/com/zhang/hot100/_007_.java
+++ src/com/zhang/hot100/_007_.java
@@ -1,5 +1,8 @@
 package com.zhang.hot100;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * ClassName: _007_
  * Package: com.zhang.hot100
@@ -11,8 +14,8 @@ package com.zhang.hot100;
  */
 public class _007_ {
     public static void main(String[] args) {
-
-
+        int[] nums = {6,5,3,7,10,1};
+        System.out.println(new _007_().trap(nums));
     }
 
 //    public int trap(int[] height) {
@@ -59,30 +62,65 @@ public class _007_ {
 //        return res;
 //    }
 
+//    public int trap2(int[] height) {
+//        int res = 0;
+//        int n = height.length;
+//        int left = 0;
+//        int right = n-1;
+//        int l_m = 0;
+//        int r_m = 0;
+//        while (left < right){
+//            l_m = Math.max(l_m, height[left]);
+//            r_m = Math.max(r_m, height[right]);
+//
+//            if(l_m < r_m){
+//                res += l_m - height[left];
+//
+//                left++;
+//            }else{
+//                res += r_m - height[right];
+//
+//                right--;
+//            }
+//
+//        }
+//        return res;
+//    }
+
+
     public int trap(int[] height) {
         int res = 0;
-        int n = height.length;
-        int left = 0;
-        int right = n-1;
-        int l_m = 0;
-        int r_m = 0;
-        while (left < right){
-            l_m = Math.max(l_m, height[left]);
-            r_m = Math.max(r_m, height[right]);
+        if(height.length < 2){
+            return 0;
+        }
 
-            if(l_m < r_m){
-                res += l_m - height[left];
+        // 单调递增栈，存下标
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(0);
 
-                left++;
+        for (int i = 1; i < height.length; i++) {
+            if(height[i] < height[stack.peek()]){
+                stack.push(i);
+            }else if(height[i] == height[stack.peek()]){
+                stack.pop();
+                stack.push(i);
             }else{
-                res += r_m - height[right];
-
-                right--;
+                while (!stack.isEmpty() && height[i] > height[stack.peek()]){
+                    int mid = stack.pop();
+                    if(!stack.isEmpty()){
+                        int left = stack.peek();
+                        int right = i;
+                        res += (right - left - 1) * (Math.min(height[left], height[right]) - height[mid]);
+                    }
+                }
+                stack.push(i);
             }
-
         }
         return res;
     }
+
+
+
 
 
 }
