82f10878aea485cbeef56bc632a88595b52001fa
hzvw <zhanghz1120@gmail.com>
2025-02-23 01:07:36 +0800

    hot100-字母异位词分组

--- src/com/zhang/hot100/_002_.java
+++ src/com/zhang/hot100/_002_.java
@@ -21,28 +21,17 @@ public class _002_ {
     }
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < strs.length; i++) {
-            map.put(strs[i], i);
-        }
-
-        HashMap<String, List<Integer>> map2 = new HashMap<>();
+        HashMap<String, List<String>> map2 = new HashMap<>();
         for(String str : strs){
             char[] cs = str.toCharArray();
             Arrays.sort(cs);
-
             String n_str = new String(cs);
             map2.putIfAbsent(n_str, new ArrayList<>());
-            map2.get(n_str).add(map.get(str));
+            map2.get(n_str).add(str);
         }
         List<List<String>> res = new ArrayList<>();
         for (String n_str : map2.keySet()) {
-            List<String> list = new ArrayList<>();
-            List<Integer> ids = map2.get(n_str);
-            for (Integer id : ids) {
-                list.add(strs[id]);
-            }
-            res.add(list);
+            res.add(map2.get(n_str));
         }
 
         return  res;
