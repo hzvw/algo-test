213dd0f069aedb69558d16a0343c8e553ad0af86
hzvw <zhanghz1120@gmail.com>
2025-02-24 20:51:37 +0800

    hot100-寻找旋转排序数组中的最小值

--- src/com/zhang/hot100/_067_.java
+++ src/com/zhang/hot100/_067_.java
@@ -11,30 +11,34 @@ package com.zhang.hot100;
  */
 public class _067_ {
     public static void main(String[] args) {
-
-
+        int[] nums = {2,3,4,5,1};
+        new _067_().findMin(nums);
 
     }
 
     public int findMin(int[] nums) {
-        int len = nums.length;
-        if (len == 0) {
-            throw new IllegalArgumentException("数组为空，无最小元素");
-        }
-        int left = 0;
-        int right = len - 1;
-        while (left < right) {
-            // int mid = left + (right - left) / 2;
-            int mid = (left + right) >>> 1;
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            } else {
-                // 因为题目中说：你可以假设数组中不存在重复元素。
-                // 此时一定有 nums[mid] < nums[right]
-                right = mid;
-            }
-        }
-        // 一定存在最小元素，因此无需再做判断
-        return nums[left];
+        return findMin(nums, 0, nums.length-1);
     }
+
+    int findMin(int[] nums, int left, int right){
+        if(left == right){
+            return nums[left];
+        }
+
+        if(left > right){
+            return Integer.MAX_VALUE;
+        }
+
+        if(right - left == 1){
+            return Math.min(nums[left], nums[right]);
+        }
+
+        int mid = left + (right - left)/2;
+        if(nums[mid] > nums[right]){
+            return Math.min(nums[left], findMin(nums, mid, right));
+        }else{
+            return Math.min(nums[mid], findMin(nums, left, mid-1));
+        }
+    }
+
 }
