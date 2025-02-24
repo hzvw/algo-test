773c899eb85e872c5437b6241a56aac75a20165a
hzvw <zhanghz1120@gmail.com>
2025-02-25 00:46:01 +0800

    hot100-接雨水-单调栈

--- src/com/zhang/hot100/_007_.java
+++ src/com/zhang/hot100/_007_.java
@@ -18,75 +18,6 @@ public class _007_ {
         System.out.println(new _007_().trap(nums));
     }
 
-//    public int trap(int[] height) {
-//        int res = 0;
-//        int n = height.length;
-//        for (int i = 0; i < n; i++) {
-//            int l_m = 0;
-//            for (int j = 0; j < i; j++) {
-//                l_m = Math.max(l_m, height[j]);
-//            }
-//            int r_m = 0;
-//            for (int j = i+1; j < n; j++) {
-//                r_m = Math.max(r_m, height[j]);
-//            }
-//            int t = Math.min(l_m, r_m) - height[i];
-//            res += t > 0 ? t : 0;
-//        }
-//        return res;
-//    }
-
-//    public int trap(int[] height) {
-//        int res = 0;
-//        int n = height.length;
-//        int[] lm = new int[n];
-//        int[] rm = new int[n];
-//        int l = 0;
-//        for (int i = 0; i < n; i++) {
-//            l = Math.max(l, height[i]);
-//            lm[i] = l;
-//        }
-//
-//        int r = 0;
-//        for (int i = n-1; i >=0 ; i--) {
-//            r = Math.max(r, height[i]);
-//            rm[i] = r;
-//        }
-//
-//        for (int i = 0; i < n; i++) {
-//            if(height[i] < Math.min(lm[i], rm[i])){
-//                res += Math.min(lm[i], rm[i]) - height[i];
-//            }
-//        }
-//
-//        return res;
-//    }
-
-//    public int trap2(int[] height) {
-//        int res = 0;
-//        int n = height.length;
-//        int left = 0;
-//        int right = n-1;
-//        int l_m = 0;
-//        int r_m = 0;
-//        while (left < right){
-//            l_m = Math.max(l_m, height[left]);
-//            r_m = Math.max(r_m, height[right]);
-//
-//            if(l_m < r_m){
-//                res += l_m - height[left];
-//
-//                left++;
-//            }else{
-//                res += r_m - height[right];
-//
-//                right--;
-//            }
-//
-//        }
-//        return res;
-//    }
-
 
     public int trap(int[] height) {
         int res = 0;
@@ -94,27 +25,17 @@ public class _007_ {
             return 0;
         }
 
-        // 单调递增栈，存下标
         Deque<Integer> stack = new LinkedList<>();
-        stack.push(0);
-
-        for (int i = 1; i < height.length; i++) {
-            if(height[i] < height[stack.peek()]){
-                stack.push(i);
-            }else if(height[i] == height[stack.peek()]){
-                stack.pop();
-                stack.push(i);
-            }else{
-                while (!stack.isEmpty() && height[i] > height[stack.peek()]){
-                    int mid = stack.pop();
-                    if(!stack.isEmpty()){
-                        int left = stack.peek();
-                        int right = i;
-                        res += (right - left - 1) * (Math.min(height[left], height[right]) - height[mid]);
-                    }
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]){
+                int mid = stack.pop();
+                if(!stack.isEmpty()){
+                    int left = stack.peek();
+                    int right = i;
+                    res += (right - left - 1) * (Math.min(height[left], height[right]) - height[mid]);
                 }
-                stack.push(i);
             }
+            stack.push(i);
         }
         return res;
     }
