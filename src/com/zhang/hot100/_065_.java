cf23fd4a3ddd7395d6c3dd94b0b7027fde655850
hzvw <zhanghz1120@gmail.com>
2025-02-25 15:50:14 +0800

    hot100-自定义优先队列

--- src/com/zhang/hot100/_065_.java
+++ src/com/zhang/hot100/_065_.java
@@ -17,48 +17,85 @@ public class _065_ {
         int[] nums = {5,7,7,8,8,10};
         int target = 8;
 
-        new _065_().searchRange(nums, target);
+        System.out.println(new _065_().searchRange(nums, target));
 
 
     }
 
     public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        if(n == 0){
-            return new int[]{-1,-1};
-        }
-        int end = -2;
+        return new int[]{searchLeft(nums, target), searchRight(nums, target)};
+    }
+
+    int searchLeft(int[] nums, int target){
+        int res = -1;
         int left = 0;
-        int right = n-1;
-        while (left  <= right){
-            int mid = left + (right-left)/2;
-            if(nums[mid] > target){
+        int right = nums.length-1;
+        while (left <= right){
+            int mid = (left + right ) >> 1;
+            if(nums[mid] >= target){
+                res = mid;
                 right = mid-1;
             }else{
-                left = mid + 1;
-                end = left;
+                left = mid+1;
             }
         }
-        int start = -2;
-        left = 0;
-        right = n-1;
+        return res;
+    }
+
+    int searchRight(int[] nums, int target){
+        int res = -1;
+        int left = 0;
+        int right = nums.length-1;
         while (left <= right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] < target){
+            int mid = (left + right ) >> 1;
+            if(nums[mid] <= target){
+                res = mid;
                 left = mid+1;
             }else{
                 right = mid-1;
-                start = right;
             }
         }
-        if(start == -2 || end == -2){
-            return new int[]{-1, -1};
-        }
-        if(end - start > 1){
-            return new int[]{start+1, end-1};
-        }
-        return new int[]{-1, -1};
+        return res;
     }
+
+
+//    public int[] searchRange(int[] nums, int target) {
+//        int n = nums.length;
+//        if(n == 0){
+//            return new int[]{-1,-1};
+//        }
+//        int end = -2;
+//        int left = 0;
+//        int right = n-1;
+//        while (left  <= right){
+//            int mid = left + (right-left)/2;
+//            if(nums[mid] > target){
+//                right = mid-1;
+//            }else{
+//                left = mid + 1;
+//                end = left;
+//            }
+//        }
+//        int start = -2;
+//        left = 0;
+//        right = n-1;
+//        while (left <= right){
+//            int mid = left + (right - left)/2;
+//            if(nums[mid] < target){
+//                left = mid+1;
+//            }else{
+//                right = mid-1;
+//                start = right;
+//            }
+//        }
+//        if(start == -2 || end == -2){
+//            return new int[]{-1, -1};
+//        }
+//        if(end - start > 1){
+//            return new int[]{start+1, end-1};
+//        }
+//        return new int[]{-1, -1};
+//    }
 
 //    public int searchInsert(int[] nums, int target) {
 //        int left = 0;
