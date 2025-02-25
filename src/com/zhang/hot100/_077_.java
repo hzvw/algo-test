a0a585a1bc447b4c3c8db87e38bb441499226913
hzvw <zhanghz1120@gmail.com>
2025-02-25 18:47:49 +0800

    hot100-打家劫舍-动态规划

--- src/com/zhang/hot100/_077_.java
+++ src/com/zhang/hot100/_077_.java
@@ -30,8 +30,12 @@ public class _077_ {
         dp[0][1] = 0;
 
         for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], -prices[i]);
+            //买卖股票的最佳时机一，只能买卖一次，所以是0-price[i]
+            dp[i][0] = Math.max(dp[i-1][0], 0-prices[i]);
             dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i]);
+            // 买卖股票最佳时机二，可以买卖多次
+            //dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]-prices[i]);
+            //dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i]);
         }
 
 //        for (int i = 0; i < n; i++) {
--- src/com/zhang/hot100/_083_.java
+++ src/com/zhang/hot100/_083_.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
  */
 public class _083_ {
 
-    public int rob(int[] nums) {
+    public int rob1(int[] nums) {
         if(nums == null || nums.length == 0){
             return 0;
         }
@@ -30,5 +30,25 @@ public class _083_ {
         }
 
         return dp[n-1];
+    }
+
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int n = nums.length;
+        if(n < 2){
+            return nums[0];
+        }
+
+        int[][] dp = new int[n][2];
+        dp[0][0] = nums[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i-1][1] + nums[i];
+            dp[i][1] = Math.max(dp[i-1][0], dp[i-1][1]);
+        }
+        return Math.max(dp[n-1][0], dp[n-1][1]);
     }
 }
