3759bdd124496a7bddc6ad5c4c5def7df2cd15f0
hzvw <zhanghz1120@gmail.com>
2025-03-02 01:06:55 +0800

    od200-最多提取子串数目

--- src/com/zhang/od/_117_.java
+++ src/com/zhang/od/_117_.java
@@ -17,7 +17,7 @@ import java.util.stream.Stream;
  * @Version 1.0
  */
 public class _117_ {
-    public static void main01(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String A = sc.nextLine();
         String B = sc.nextLine();
@@ -45,92 +45,12 @@ public class _117_ {
                 cnt++;
             }
 
-            if (i == ca.length && j < cb.length) {
+            if (i == ca.length /*&& j < cb.length*/) {
                 break;
             }
         }
 
         System.out.println(cnt);
 
-    }
-
-    public static void main(String[] args) {
-        p();
-
-    }
-
-    static void p() {
-        //创建，1，通过集合
-        List<Integer> arrayList = new ArrayList<>();
-        Stream<Integer> stream = arrayList.parallelStream();
-//
-//        //2.通过数组
-//        Integer[] arr = new Integer[10];
-//        Stream<Integer> stream1 = Arrays.stream(arr);
-//
-//        System.out.println("stream.filter(x -> x > 100) = " + stream.filter(x -> x > 100));
-//
-//        System.out.println("stream1 = " + stream1);
-//
-//        System.out.printf("", stream);
-//
-//        stream.distinct();
-
-        //终止操作和中间操作
-
-        //3.通过of方法
-        Stream<String> stringStream = Stream.of("a a", "b b");
-        List<String> list = stringStream.flatMap(x -> Arrays.stream(x.split(" "))).collect(Collectors.toList());
-        list.forEach(System.out::println);
-
-
-//        System.out.println(stringStream.reduce(_117_::function4).get());
-
-//        System.out.println(stringStream.reduce(String::concat).get());
-//        System.out.println("stringStream.allMatch(_117_::function3) = " + stringStream.allMatch(_117_::function3));
-
-//        System.out.println(stringStream.count());
-
-//        System.out.println(stringStream.min((a, b) -> a.compareTo(b)).get());
-        //stringStream.map(x-> x.toLowerCase()).flatMap(x -> Arrays.stream(x.split(" "))).forEach(System.out::println);
-
-        //stringStream.sorted((a,b) -> b.compareTo(a)).forEach(System.out::println);
-
-        //匹配查找
-        //规约
-        //
-
-
-        System.out.println(Stream.of(1.1, 1.2).min(Double::compare).get());
-
-
-        //1.筛选切片
-
-
-        //2.映射
-        //3.排序
-        //收集
-        //collect
-
-
-
-
-    }
-
-    static String function(String input){
-        return input.toLowerCase();
-    }
-
-    static Stream<String> function2(String input){
-        String[] words = input.split(" ");
-        return Arrays.stream(words);
-    }
-
-    static boolean function3(String str){
-        return false;
-    }
-
-    static String function4(String s1 , String s2){
-        return s1+s2;
     }
 }
