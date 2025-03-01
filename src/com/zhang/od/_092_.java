f50ad2c00846bb6a6b201321ba8bc266aa8ce225
hzvw <zhanghz1120@gmail.com>
2025-03-01 17:10:09 +0800

    od200-mvp争夺战-球放桶问题

--- src/com/zhang/od/_092_.java
+++ src/com/zhang/od/_092_.java
@@ -26,42 +26,38 @@ public class _092_ {
             sum += buffer.get(i);
         }
 
-        int i = m;
-        for(;i >= 1;i--){
-            if(sum % i != 0){
+        int k = m;//桶的数量
+        for(;k >= 1;k--){
+            if(sum % k != 0){
                 continue;
             }
-            int sub = sum / i;
-            if(buffer.get(0) > sub){
+            int subSum = sum / k;
+            if(buffer.get(0) > subSum){
                 continue;
             }
 
-            int[] buckets = new int[i];
-            if(canPartition(sub, buffer, 0, buckets)){
-                System.out.println(sub);
+            int[] buckets = new int[k];
+            if(canPartition(subSum, buffer, 0, buckets)){
+                System.out.println(subSum);
                 return;
             }
         }
     }
 
-    static boolean canPartition(int sub, List<Integer> buffer, int j , int[] buckets){
-        if(j == buffer.size()){
+    static boolean canPartition(int sub, List<Integer> nums, int index , int[] buckets){
+        if(index == nums.size()){
             return true;
         }
-        int last = buckets[0];
-        int select = buffer.get(j);
-        for(int k = 0; k<buckets.length; k++){
-            if(k > 0 && buckets[k] == last){
+        int selected = nums.get(index);
+        for(int i = 0; i<buckets.length; i++){
+            if(i > 0 && buckets[i] == buckets[i-1]){
                 continue;
             }
-            if(buckets[k] + select <= sub){
-                buckets[k] += select;
-                if(canPartition(sub, buffer, j+1, buckets)){
-                    return true;
-                }
-                buckets[k] -= select;
+            if(buckets[i] + selected <= sub){
+                buckets[i] += selected;
+                if(canPartition(sub, nums, index+1, buckets)){return true;}
+                buckets[i] -= selected;
             }
-            last = buckets[k];
         }
         return false;
     }
