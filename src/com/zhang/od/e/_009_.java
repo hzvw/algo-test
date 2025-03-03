5d8d0b35d7a07c57372218ac491405291d4f27d4
hzvw <zhanghz1120@gmail.com>
2025-03-03 18:24:43 +0800

    od200-模拟文件系统

--- src/com/zhang/od/e/_009_.java
+++ src/com/zhang/od/e/_009_.java
@@ -12,91 +12,92 @@ import java.util.*;
  * @Version 1.0
  */
 public class _009_ {
+    static class Node{
+        String name;
+        Node fa;
+        List<Node> ch;
 
-
-    static class TreeNode{
-        String dir;
-        TreeNode fa;
-
-        List<TreeNode> ch = null;
-
-        public TreeNode(String dir, TreeNode fa, List<TreeNode> ch) {
-            this.dir = dir;
+        public Node(String name, Node fa, List<Node> ch) {
+            this.name = name;
             this.fa = fa;
             this.ch = ch;
         }
     }
 
     static class FS{
-        TreeNode root = new TreeNode("/", null, new ArrayList<>());
-
-        TreeNode cur = null;
+        Node root = new Node("/", null, new ArrayList<>());
+        Node cur = root;
 
         public FS() {
-            cur = root;
         }
 
-        public void mkdir(String dir_name){
-            TreeNode t = new TreeNode(dir_name + "", cur, new ArrayList<>());
+        void mkdir(String name){
+            Node t = new Node(name, cur, new ArrayList<>());
             cur.ch.add(t);
         }
 
-        public void cd(String dir_name){
-            if(dir_name.equals("..")){
-               cur = cur.fa;
+        void cd(String name){
+            if(name.equals("..")){
+                if(cur != root) {cur = cur.fa;}
             }else{
-                for(TreeNode treeNode : cur.ch){
-                    if(dir_name.equals(treeNode.dir)){
-                        cur = treeNode;
+                for (Node node : cur.ch) {
+                    if(node.name.equals(name)){
+                        cur = node;
                         break;
                     }
                 }
+
+
             }
+
+
         }
 
-        public void pwd(){
-            LinkedList<TreeNode> queue = new LinkedList<>();
-            queue.addFirst(cur);
-
-            List<String> names = new ArrayList<>();
-            while(!queue.isEmpty()){
-                TreeNode t = queue.removeLast();
-
-                names.add(t.dir);
-                TreeNode fa = t.fa;
-                if(fa != null){
-                    queue.addFirst(fa);
-                }
+        String pwd(){
+            List<String> list = new ArrayList<>();
+            Node p = cur;
+            while (p != null){
+                list.add(p.name);
+                p = p.fa;
             }
-            Collections.reverse(names);
+            Collections.reverse(list);
+
             StringBuilder sb = new StringBuilder("/");
-            for(int i = 1; i<names.size(); i++){
-                sb.append(names.get(i) + "/");
+            for (int i = 1; i < list.size(); i++) {
+                sb.append(list.get(i));
+                sb.append("/");
             }
-            System.out.print(sb.toString());
+            return sb.toString();
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        List<String> commands = new ArrayList<>();
-        while(sc.hasNextLine()){
-            commands.add(sc.nextLine());
+        FS fs = new FS();
+        List<String> list = new ArrayList<>();
+        while (sc.hasNextLine()){
+            String comm = sc.nextLine();
+            list.add(comm);
         }
 
-        FS fs = new FS();
-        for(String command : commands){
-            if(command.startsWith("mkdir")){
-                String[] words = command.split(" ");
+        String last = null;
+        for (String comm : list) {
+            if(comm.startsWith("mkdir")){
+                String[] words = comm.split(" ");
                 fs.mkdir(words[1]);
-            }else if(command.startsWith("cd")){
-                String[] words = command.split(" ");
+                last = "/";
+            }else if(comm.startsWith("cd")){
+                String[] words = comm.split(" ");
                 fs.cd(words[1]);
+                last = "/";
             }else{
-                fs.pwd();
+                last = fs.pwd();
             }
         }
+        System.out.println(last);
+
     }
+
+
 
 }
