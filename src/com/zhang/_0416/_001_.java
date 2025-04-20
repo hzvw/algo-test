f6331ffb0684d8d7c212812724c169129153423d
hzvw <zhanghz1120@gmail.com>
2025-04-21 00:57:18 +0800

    华为od机考第一题，先用正则表达式提取有效的表达式，然后使用栈来计算结果，当时没做出来

--- src/com/zhang/_0416/_001_.java
+++ src/com/zhang/_0416/_001_.java
@@ -14,6 +14,7 @@ import java.util.regex.Pattern;
  * @Version 1.0
  */
 public class _001_ {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
