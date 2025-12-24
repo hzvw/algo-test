aff344f70b202d8ba3636a3d6e86d02be90815fe
hzvw <zhanghz1120@foxmail.com>
2025-12-24 23:34:05 +0800

    提交代码

---
+++ src/main/java/com/zhang/concurrent/Main.java
@@ -0,0 +1,13 @@
+package com.zhang.concurrent;
+
+/**
+ *
+ * @Author Harizon
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("你好");
+
+    }
+}
